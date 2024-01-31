5bc0cafa0ec0f60df14cac6257543105ef926dfe
siddhi Algude <algudesiddhi04@gmail.com>
2024-01-31 22:58:06 +0530

    setup built

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express=require("express");
+const bodyparser=require("body-parser");
 const{port}=require("./config/serverconfig")
+
 const setupAndStartServer=async()=>{
   //create an express object
   const app=express();
-   
+   app.use(bodyparser.json());
+   app.use(bodyparser.urlencoded({extended:true}));
   app.listen(port,()=>{
     console.log(`Server started at ${port}`);
     //console.log(process.env);
